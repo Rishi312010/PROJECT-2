b7a478b238028e8ab879bb20038c49a7e9861a90
Rishi312010 <104554473+Rishi312010@users.noreply.github.com>
2022-06-13 19:42:29 +0530

    Add files via upload

---
+++ desk.js
@@ -0,0 +1,26 @@
+img = ""
+
+
+function preload(){
+    img = loadImage('Desk.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img , gotResults);
+}
+
+function gotResults( error , results){
+    if (error)  {
+        console.log(error);
+    }
+    console.log(results);
+}
